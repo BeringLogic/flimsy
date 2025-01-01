064bc8b0ca8f7da21f81e513ef0d62f6cbc82b12
Philippe Gouin <bering@ringlogic.com>
2025-01-01 11:29:35 -0500

    quick index.php to start with

---
+++ src/index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+  <title>Flimsy</title>
+  <style>
+    div.block {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      justify-content: space-evenly;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>Flimsy Home Page</h1>
+
+  <h2>Block 1</h2>
+  <div class="block">
+    <div class="item">Item 1</div>
+    <div class="item">Item 2</div>
+    <div class="item">Item 3</div>
+    <div class="item">Item 4</div>
+    <div class="item">Item 5</div>
+    <div class="item">Item 6</div>
+    <div class="item">Item 7</div>
+    <div class="item">Item 8</div>
+    <div class="item">Item 9</div>
+  </div>
+
+  <h2>Block 2</h2>
+  <div class="block">
+    <div class="item">Item 1</div>
+    <div class="item">Item 2</div>
+    <div class="item">Item 3</div>
+    <div class="item">Item 4</div>
+    <div class="item">Item 5</div>
+    <div class="item">Item 6</div>
+    <div class="item">Item 7</div>
+    <div class="item">Item 8</div>
+    <div class="item">Item 9</div>
+  </div>
+
+</body>
+</html>
