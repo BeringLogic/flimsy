486594b2c69f58bd48b72825925fc1247d58adfc
Philippe Gouin <bering@ringlogic.com>
2025-01-07 19:28:55 -0500

    Added a bit of error handling to getSystemInfo.php

--- src/getSystemInfo.php
+++ src/getSystemInfo.php
@@ -7,11 +7,15 @@ $result = array();
 
 $result['cpu_temp'] = exec('sensors | grep "'.$config["cpu_temp_sensor"].'" | cut -d ":" -f 2 | awk \'{ print $1 }\'');
 $result['free_memory'] = exec('free -h | grep Mem | awk \'{ print $4 }\'');
-$result['free_swap'] = exec('free -h | tail -n 1 | awk \'{ print $4 }\'');
+$result['free_swap'] = exec('free -h | grep Swap | awk \'{ print $4 }\'');
 $result['disks'] = array();
 
 if (!empty($config['mount_points'])) {
   foreach (explode(',', $config['mount_points']) as $mount_point) {
+    if (!is_dir($mount_point)) {
+      error_log("ERROR: Invalid mount point: " . $mount_point . ". It doesn't exists or user www-data doesn't have permission to reach it.");
+      continue;
+    }
     $result['disks'][] = array(
       'mount_point' => basename($mount_point) ?: '/',
       'free_disk_space' => exec("df -h " . $mount_point . " | tail -n 1 | awk '{ print $4 }'")
