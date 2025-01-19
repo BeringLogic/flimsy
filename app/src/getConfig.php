d210e5fbc9c8cc6a1dd555c544fd18d9336f3c4b
Philippe Gouin <bering@ringlogic.com>
2025-01-19 17:40:30 -0500

    dev leftovers

--- app/src/getConfig.php
+++ app/src/getConfig.php
@@ -4,15 +4,9 @@ require('db.php');
 $db = new db();
 
 $result = $db->GetConfig();
-// $result['sensors'] = array();
+
 exec("sensors -jA", $output);
 $result['sensors'] = json_decode(implode('', $output), true);
-// foreach($output as $line) {
-  // $result['sensors'][] = array(
-  //   'text' => $line,
-  //   'value' => substr($line, 0, strpos($line, ':'))
-  // );
-// }
 
 header('Content-Type: application/json');
 echo json_encode($result);
