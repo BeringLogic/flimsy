1cc680d306adb46bbedec77e5022c0d0972cb7ce
Philippe Gouin <bering@ringlogic.com>
2025-02-22 12:12:07 -0500

    WIP post config

--- src/internal/db/config.go
+++ src/internal/db/config.go
@@ -39,3 +39,35 @@ func (flimsyDB *FlimsyDB) LoadConfig() (*Config, error) {
 
   return &config, nil
 }
+
+func (flimsyDB *FlimsyDB) SaveConfig(config *Config) error {
+  _, err := flimsyDB.sqlDb.Exec(
+    `UPDATE config SET
+      icon = ?,
+      title = ?,
+      background_image = ?,
+      color_background = ?,
+      color_foreground = ?,
+      color_items = ?,
+      color_borders = ?,
+      cpu_temp_sensor = ?,
+      show_free_ram = ?,
+      show_free_swap = ?,
+      show_public_ip = ?,
+      show_free_space = ?
+    WHERE id = 1`,
+    config.Icon,
+    config.Title,
+    config.Background_image,
+    config.Color_background,
+    config.Color_foreground,
+    config.Color_items,
+    config.Color_borders,
+    config.Cpu_temp_sensor,
+    config.Show_free_ram,
+    config.Show_free_swap,
+    config.Show_public_ip,
+    config.Show_free_space)
+
+  return err
+}
